5520aea127c68f12a9d3344fc39fa7066fa90e73
cmroxv-pixel <cmroxv@gmail.com>
2026-04-14 17:34:35 +1000

    Update AppLayout.tsx

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -12,7 +12,7 @@ export default function AppLayout({ children }: AppLayoutProps) {
   return (
     <div className="min-h-screen bg-background flex flex-col">
       <Topbar />
-      <main className="flex-1 w-full max-w-screen-2xl mx-auto px-4 lg:px-8 xl:px-10 2xl:px-16 py-6 pb-20">
+      <main className="flex-1 w-full px-0 py-0 pb-20">
         {children}
       </main>
       <BottomNav />
