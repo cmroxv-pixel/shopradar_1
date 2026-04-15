54d08f0b2972643d504e3c999e31657b37e6dc12
cmroxv-pixel <cmroxv@gmail.com>
2026-04-15 14:07:14 +1000

    Update DarkModeInit.tsx

--- src/components/DarkModeInit.tsx
+++ src/components/DarkModeInit.tsx
@@ -1,17 +1,26 @@
+import Script from 'next/script';
+
 export default function DarkModeInit() {
-  const script = `
-    (function() {
-      try {
-        var saved = localStorage.getItem('shopradar-mode');
-        if (saved === 'light') {
-          document.documentElement.removeAttribute('data-mode');
-        } else {
-          document.documentElement.setAttribute('data-mode', 'dark');
-        }
-      } catch(e) {
-        document.documentElement.setAttribute('data-mode', 'dark');
-      }
-    })();
-  `;
-  return <script dangerouslySetInnerHTML={{ __html: script }} />;
+  return (
+    <Script
+      id="dark-mode-init"
+      strategy="beforeInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          (function() {
+            try {
+              var saved = localStorage.getItem('shopradar-mode');
+              if (saved === 'light') {
+                document.documentElement.removeAttribute('data-mode');
+              } else {
+                document.documentElement.setAttribute('data-mode', 'dark');
+              }
+            } catch(e) {
+              document.documentElement.setAttribute('data-mode', 'dark');
+            }
+          })();
+        `,
+      }}
+    />
+  );
 }
